191a14520711a57dcc6b0d43053cbf878e4d71da
Karo <karoogorek@gmail.com>
2024-06-25 12:01:25 +0200

    pokemon list added

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-alert('Hello world!');
-
-let favoriteFood = 'Ramen';
-document.write(favoriteFood);
+let pokemonList = [
+    { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison'] },
+    {name: 'Charmander', height: 0.6, types: ['fire']},
+    {name: 'Squirtle', height: 0.5, types: ['water']},
+];
