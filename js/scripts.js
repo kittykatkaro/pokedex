cbb28416140caa52535a960bd8179a0cb001717f
Karo <karoogorek@gmail.com>
2024-06-26 21:39:08 +0200

    Pokedex listing is added in `scripts.js` to display each Pokemon's name and height;
    added basic CSS styles

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,10 @@ let pokemonList = [
     {name: 'Charmander', height: 0.6, types: ['fire']},
     {name: 'Squirtle', height: 0.5, types: ['water']},
 ];
+for (let i = 0; i < pokemonList.length; i++) {
+ let pokemon = pokemonList[i];
+ document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ')');
+ if (pokemon.height > 0.6) {
+   document.write ('  - Wow, that’s big!');
+ }
+}
