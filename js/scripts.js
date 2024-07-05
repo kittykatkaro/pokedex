53bf7fd61f2db4c5d9608c2e746623d850e2c591
Karo <karoogorek@gmail.com>
2024-07-05 13:26:02 +0200

    wrap pokemonList in IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,26 @@
+let pokemonRepository = (function () {
+
 let pokemonList = [
     { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison'] },
     {name: 'Charmander', height: 0.6, types: ['fire']},
     {name: 'Squirtle', height: 0.5, types: ['water']},
 ];
 
-pokemonList.forEach(function(pokemon) {
+function getAll() {
+  return pokemonList;
+}
+
+function add(pokemon) {
+  pokemonList.push(pokemon);
+}
+
+return {
+  getAll: getAll,
+  add: add
+};
+})();
+
+pokemonRepository.getAll().forEach(function(pokemon) {
     document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ')');
     if (pokemon.height > 0.6) {
         document.write ('  - Wow, that’s big!');
