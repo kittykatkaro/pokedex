4f0991ce74102488eba15bdb6ec29a4ff6f1706b
Karo <karoogorek@gmail.com>
2024-07-05 17:02:11 +0200

    Update CSS styles and add Pokemon list display in Pokedex

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,48 @@
 let pokemonRepository = (function () {
 
-let pokemonList = [
+  let pokemonList = [
     { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison'] },
-    {name: 'Charmander', height: 0.6, types: ['fire']},
-    {name: 'Squirtle', height: 0.5, types: ['water']},
-];
+    { name: 'Charmander', height: 0.6, types: ['fire'] },
+    { name: 'Squirtle', height: 0.5, types: ['water'] },
+  ];
 
-function getAll() {
-  return pokemonList;
-}
+  function getAll() {
+    return pokemonList;
+  }
 
-function add(pokemon) {
-  pokemonList.push(pokemon);
-}
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
 
-return {
-  getAll: getAll,
-  add: add
-};
+  function addListItem(pokemon) {
+    let ul = document.querySelector('ul');
+
+    let listItem = document.createElement('li');
+    let button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add('pokemon-button');
+
+    button.addEventListener('click', function () {
+      showDetails(pokemon);
+    });
+
+    listItem.appendChild(button);
+    ul.appendChild(listItem);
+  }
+
+  function showDetails(pokemon) {
+    console.log(pokemon.name);
+  }
+
+  return {
+    getAll: getAll,
+    add: add,
+    addListItem: addListItem
+
+  };
 })();
 
-pokemonRepository.getAll().forEach(function(pokemon) {
-    document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ')');
-    if (pokemon.height > 0.6) {
-        document.write ('  - Wow, that’s big!');
-    }
+pokemonRepository.getAll().forEach(function (pokemon) {
+  pokemonRepository.addListItem(pokemon);
+
 });
